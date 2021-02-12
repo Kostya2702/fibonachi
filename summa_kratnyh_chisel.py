24e2aad5c6b59d8da2a39781a337e3022164af10
Kostya2702 <loshkarev.kostya27@gmail.com>
2021-02-12 20:37:05 +0500

    Add files via upload

---
+++ summa_kratnyh_chisel.py
@@ -0,0 +1,16 @@
+
+def fibonachi(elements):
+    result = 2
+    for i in range(elements):
+        for n in range(11):
+            print(n)
+            # result += n
+        # result += i
+
+#     result = 2
+#     for i in range(2, n):
+#         if i % 2 == 0:
+#             result += i
+    # print(result)
+#
+fibonachi(10)
